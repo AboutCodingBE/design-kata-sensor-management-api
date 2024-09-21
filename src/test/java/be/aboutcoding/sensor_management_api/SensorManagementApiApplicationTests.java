5c22fa5d002d96c80c18a09d2de564c0a8cd32e5
Nicholas Ocket <nicholas@aboutcoding.be>
2024-09-21 17:01:45 +0200

    first commit

---
+++ src/test/java/be/aboutcoding/sensor_management_api/SensorManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package be.aboutcoding.sensor_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
